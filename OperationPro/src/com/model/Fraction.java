21d2158d650c21822f11b916c43abc3cb9e5da02
linhao <linhao0056@qq.com>
2018-09-21 17:55:49 +0800

    sdf

--- OperationPro/src/com/model/Fraction.java
+++ OperationPro/src/com/model/Fraction.java
@@ -31,10 +31,6 @@ public class Fraction {
 	
 	
 	
-	@Override
-	public String toString() {
-		return "Fraction [denominator=" + denominator + ", nominator=" + nominator + "]";
-	}
 
 	// 加法运算
 	Fraction add(Fraction r){  
