44b1ffe91bb4f584293ca04aabe46fcd03f40ca0
linhao <linhao0056@qq.com>
2018-09-21 17:53:00 +0800

    第二

--- OperationPro/src/com/model/Fraction.java
+++ OperationPro/src/com/model/Fraction.java
@@ -1,9 +1,11 @@
 package com.model;
 
+
+
 public class Fraction {
 
-	private int denominator;
-	private int nominator;
+	private int denominator;//分母
+	private int nominator;//分子
 	
 	public Fraction(int denominator, int nominator) {
 		super();
@@ -27,4 +29,51 @@ public class Fraction {
 		this.nominator = nominator;
 	}
 	
+	
+	
+	@Override
+	public String toString() {
+		return "Fraction [denominator=" + denominator + ", nominator=" + nominator + "]";
+	}
+
+	// 加法运算
+	Fraction add(Fraction r){  
+		int a = r.getNominator();//获得分子
+		int b = r.getDenominator();//获得分母
+		int newNominator = nominator * b + denominator * a;
+		int newDenominator = denominator * b;
+		Fraction result = new Fraction(newNominator,newDenominator);
+		return result;
+	}
+	
+	
+	// 减法运算
+	Fraction sub(Fraction r){  
+		int a = r.getNominator();//获得分子
+		int b = r.getDenominator();//获得分母
+		int newNominator = nominator * b - denominator * a;
+		int newDenominator = denominator * b;
+		Fraction result = new Fraction(newNominator,newDenominator);
+		return result;
+	} 
+	
+	//分数的乘法运算
+	Fraction muti(Fraction r){ // 乘法运算
+		int a = r.getNominator();//获得分子
+		int b = r.getDenominator();//获得分母
+		int newNominator = nominator * a;
+		int newDenominator = denominator * b;
+		Fraction result = new Fraction(newNominator,newDenominator);
+		return result;
+	}
+	
+	//分数除法运算
+	Fraction div(Fraction r){  
+		int a = r.getNominator();//获得分子
+		int b = r.getDenominator();//获得分母
+		int newNominator = nominator * b;
+		int newDenominator = denominator * a;
+		Fraction result = new Fraction(newNominator,newDenominator);
+		return result;
+	}
 }
